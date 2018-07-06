9bdef1edb9f72d5ed739e1414af7460e3d45a51b
Thiago Oliveira <thiago.oliveira@newm.com.br>
2018-07-06 13:06:34 -0300

    Fix two way binding

---
+++ app/src/main/java/com/skiptirengu/dhice/util/Conv.java
@@ -0,0 +1,15 @@
+package com.skiptirengu.dhice.util;
+
+import android.databinding.InverseMethod;
+
+public class Conv {
+
+    @InverseMethod("stringToInt")
+    public static String intToString(int num) {
+        return String.valueOf(num);
+    }
+
+    public static int stringToInt(String num) {
+        return num == null || num.isEmpty() ? 0 : Integer.valueOf(num);
+    }
+}
